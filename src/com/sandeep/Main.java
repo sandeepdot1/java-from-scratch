ec0ac86d3a5cff86ccb6cad7499a7e215b8af2bd
sandeepdot1 <sandeeppanigrahi3570@gmail.com>
2023-01-09 01:59:34 +0530

    intial commit

---
+++ src/com/sandeep/Main.java
@@ -0,0 +1,21 @@
+package com.sandeep;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+            This package will contain all the necessary concepts of java,
+            and I will update this repo from time to time so that it is always
+            up-to-date.
+         */
+
+        // Very first line to start with any new language
+        System.out.println("Hello Java");
+
+        // taking input from user and printing it on screen
+        Scanner ip = new Scanner(System.in);
+        int n = ip.nextInt();
+        System.out.println(n);
+    }
+}
